74c14f4009277153aa0f8864458e10b384ee0dde
su-vely <dbtngus92@naver.com>
2025-06-25 19:41:14 +0900

    프로젝트 생성

---
+++ bin/dart_shoppingmall.dart
@@ -0,0 +1,5 @@
+import 'package:dart_shoppingmall/dart_shoppingmall.dart' as dart_shoppingmall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_shoppingmall.calculate()}!');
+}
---
+++ test/dart_shoppingmall_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_shoppingmall/dart_shoppingmall.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
